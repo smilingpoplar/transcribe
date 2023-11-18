90545873d409e83e6e21c58ad9d58725d82cf694
le <smilingpoplar@gmail.com>
2023-11-18 22:49:08 +0800

    转录出视频文本

---
+++ README.md
@@ -0,0 +1,21 @@
+将视频、音频文件或 http 链接 => 文本或字幕
+
+## MacOS
+
+### 安装
+
+```
+brew install whisper-cpp ffmpeg
+brew install yt-dlp jq
+
+git clone https://github.com/ggerganov/whisper.cpp.git
+./whisper.cpp/models/download-ggml-model.sh medium-q5_0
+```
+
+### 转录出视频文本
+
+```
+./transcribe.sh <video_file_or_http_link> [other_whisper_cpp_options]
+```
+
+支持的 http 链接 [见 yt-dlp](https://github.com/yt-dlp/yt-dlp/tree/master/yt_dlp/extractor) 、支持的参数选项 [见 whisper.cpp](https://github.com/ggerganov/whisper.cpp)
---
+++ transcribe.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+    echo "Usage: ./transcribe.sh <video_file_or_http_link> [other_whisper_cpp_options]"
+    exit 1
+fi
+
+# 指定文件或http链接
+if [[ $1 =~ ^(http|https):// ]]; then
+    dir="videos"
+    mkdir -p $dir && cd $dir
+    yt-dlp --extract-audio --audio-format wav --write-info-json "$1"
+    title=$(jq -r .title *.info.json)
+    rm *.info.json
+    file="$title.wav"
+    mv *\[*\].wav "$file"
+    cd ..
+    file="$dir/$file"
+else
+    file="$1"
+fi
+
+# ffmpeg预处理
+f=${file%.*} # 去除后缀
+ffmpeg -i "$file" -ar 16000 "$f.tmp.wav"
+mv "$f.tmp.wav" "$f"
+
+# whisper转录
+shift # 将$1移出参数列表
+export GGML_METAL_PATH_RESOURCES="whisper.cpp/"
+whisper-cpp -l auto -otxt -osrt -t 6 -m "whisper.cpp/models/ggml-medium-q5_0.bin" "$@" "$f"
+rm "$f"
