0e22d28cd3b2372df9117eb9ae4ace11c3ae4770
le <smilingpoplar@gmail.com>
2023-12-10 22:36:57 +0800

    翻译txt和srt

--- transcribe.sh
+++ transcribe.sh
@@ -29,3 +29,13 @@ shift # 将$1移出参数列表
 export GGML_METAL_PATH_RESOURCES="whisper.cpp/"
 bin/whisper-cpp -l auto -otxt -osrt -t 6 -m "whisper.cpp/models/ggml-medium-q5_0.bin" "$@" "$f"
 rm "$f"
+
+# translate翻译
+if [ -e "$f.txt" ]; then
+    echo "translating txt ..."
+    bin/translate < "$f.txt" > "$f.zh.txt"
+fi
+if [ -e "$f.srt" ]; then
+    echo "translating srt ..."
+    bin/translate < "$f.srt" | sed 's/ --&gt; / --> /g' > "$f.zh.srt"
+fi
